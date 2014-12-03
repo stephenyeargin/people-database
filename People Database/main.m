330b2b23c3bcbeae05076a3272893cbdd76b42e7
Stephen Yeargin <stephen.yeargin@gmail.com>
2014-12-03 16:43:03 -0600

    Initial Commit

---
+++ People Database/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  People Database
+//
+//  Created by Stephen Yeargin on 12/3/14.
+//  Copyright (c) 2014 Stephen Yeargin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
